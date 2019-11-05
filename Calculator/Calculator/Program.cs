8cadd7ecf3041b01404f55040432dc78fb0599ce
ShimuSourav <rdfox.007@gmail.com>
2019-11-05 11:33:50 +0600

    added files and modified

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 20;
+
+            Console.WriteLine(num1 + num2);
+
+            Console.ReadKey();
+        }
+    }
+}
